adc654c75c4f311d147ffafa7dcfcaa2240b7daa
mattbf <matthewbf8@gmail.com>
2019-03-13 15:17:48 -0600

    Added an anchor tag to link to google in a new tab

--- reactsandbox/src/Home.js
+++ reactsandbox/src/Home.js
@@ -4,6 +4,7 @@ function Home() {
   return(
     <div>
       <h2>Hello, this the the React Sandbox home page.</h2>
+      <a href="https://www.google.com" target="_blank"> Testing out Links! </a>
     </div>
   )
 }
