ee68f84ff88145312ca3247e2f55d01abd5ed726
mattbf <matthewbf8@gmail.com>
2019-03-13 16:09:38 -0600

    Added some links with react router

--- reactsandbox/src/Home.js
+++ reactsandbox/src/Home.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function About() {
+  return(
+    <div>
+      <h1>About Page</h1>
+      <Link to="/"> Home Page </Link>
+    </div>
+  )
+}
+
+function GlobalRouter() {
+  return(
+    <div>
+      <Route path="/about" component={About} />
+      <Route exact path="/" component={Home} />
+    </div>
+   )
+}
 
 function Home() {
   return(
-    <div>
-      <h2>Hello, this the the React Sandbox home page.</h2>
-      <a href="https://www.google.com" target="_blank"> Testing out Links! </a>
-    </div>
+    <GlobalRouter>
+      <div>
+        <h2>Hello, this the the React Sandbox home page.</h2>
+        <a href="https://www.google.com" target="_blank"> Testing out Links! </a>
+         <Link to="/about"> About Page </Link>
+      </div>
+    </GlobalRouter>
   )
 }
 
