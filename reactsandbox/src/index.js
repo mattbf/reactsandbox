1d8571274e2812ecdbb6e233f6e1a7dcd3b9dee8
mattbf <matthewbf8@gmail.com>
2019-03-15 08:29:15 -0600

    Installed axios and configured the baseURL globally

--- reactsandbox/src/index.js
+++ reactsandbox/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com/';
+//axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+//axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
