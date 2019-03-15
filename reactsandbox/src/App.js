eb0297042061c531daeb1f04a12ff0ec048268ca
mattbf <matthewbf8@gmail.com>
2019-03-15 07:59:27 -0600

    Separated pages with exact URL match

--- reactsandbox/src/App.js
+++ reactsandbox/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './Home.js';
-import About from './Home.js';
 
 import {
   BrowserRouter as Router,
@@ -10,6 +9,15 @@ import {
   Switch,
   Redirect
 } from 'react-router-dom'
+
+function About() {
+  return(
+    <div>
+      <h1>About Page</h1>
+      <Link to="/"> Home Page </Link>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
@@ -22,8 +30,8 @@ class App extends Component {
               <li><Link to="/about">About</Link></li>
             </ul>
             <hr/>
-            <Route path="/" component={Home} />
-            <Route path="/about" component={About} />
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
           </div>
         </div>
       </Router>
--- reactsandbox/src/Home.js
+++ reactsandbox/src/Home.js
@@ -2,20 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 
-function About() {
-  return(
-    <div>
-      <h1>About Page</h1>
-      <Link to="/"> Home Page </Link>
-    </div>
-  )
-}
+
 
 function Home() {
   return(
       <div>
         <h2>Hello, this the the React Sandbox home page.</h2>
-        <a href="https://www.google.com" target="_blank" rel="noopener noreferrer"> Testing out Links! </a>
         <Link to="/about"> About Page </Link>
       </div>
   )
