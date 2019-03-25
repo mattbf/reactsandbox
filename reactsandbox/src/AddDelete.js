77bf6f1290d8c76df7ae45bae78f079edbe598b7
mattbf <matthewbf8@gmail.com>
2019-03-25 15:54:38 -0600

    can add one component

--- reactsandbox/src/AddDelete.js
+++ reactsandbox/src/AddDelete.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import {useEffect, useState} from 'react';
 
-function AddDelete() {
+function SampleComponent(key, name) {
   return(
-    <h1> Add and Delete </h1>
+    <div>
+      <ul>
+        <li> {name} </li>
+      </ul>
+    </div>
   )
 }
 
-export default AddDelete
+function Add() {
+  const [children, setChildren] = useState(
+    [
+      {id: 1, name: "Chris", backgroundcolor: '000000'},
+      {id: 2, name: "Matt", backgroundcolor: '00C853'},
+    ]
+  )
+  const childrenList = (
+    <ul>
+      {children.map((item) =>
+        <li key={item.id}>
+          {item.name}
+        </li>
+      )}
+    </ul>
+  );
+  function addChild() {
+    setChildren(children.concat([{id: 3, name: "Matt", backgroundcolor: '00C853'}]))
+  }
+  return(
+    <div>
+      <h1> Add and Delete </h1>
+      <div>
+        {childrenList}
+      </div>
+      <button onClick={addChild}> Add Component </button>
+    </div>
+  )
+}
+
+export default Add
