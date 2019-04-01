7bcbe4a6fcb117c922d7cde6617d169919597160
mattbf <matthewbf8@gmail.com>
2019-04-01 16:22:18 -0600

    Porblem transpiling date picker

--- reactsandbox/src/DatePickerResponsive.js
+++ reactsandbox/src/DatePickerResponsive.js
@@ -1,16 +1,37 @@
-import { MuiPickersUtilsProvider } from 'material-ui-pickers';
+import React, { Fragment, useState } from "react";
+import { DatePicker, MuiPickersUtilsProvider, Calendar } from "material-ui-pickers";
+import moment from "moment";
+import MomentUtils from "@date-io/moment";
 
-// pick utils
-import MomentUtils from '@date-io/moment';
-import DateFnsUtils from '@date-io/date-fns';
-import LuxonUtils from '@date-io/luxon';
 
-function App() {
+function DatePickerResponsive(props) {
+  const [selectedDate, handleDateChange] = useState(new Date());
+
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <Root />
+    <MuiPickersUtilsProvider utils={MomentUtils} moment={moment}>
+      <Calendar
+        autoOk={false}
+        cancelLabel={false}
+        firstDayOfWeek={1}
+        mode={'portrait'}
+        open={true}
+      />
+
     </MuiPickersUtilsProvider>
   );
 }
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+export default DatePickerResponsive;
+
+/*
+
+<div className="picker">
+  <DatePicker
+    label="Basic example"
+    value={selectedDate}
+    onChange={handleDateChange}
+    animateYearScrolling
+  />
+</div>
+
+*/
