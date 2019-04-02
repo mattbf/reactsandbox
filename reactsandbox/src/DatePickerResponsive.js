4a69214459fe2b4e1d78861f4de94f28d7ad7f7d
mattbf <matthewbf8@gmail.com>
2019-04-02 15:45:09 -0600

    Got calendar and date slect component on same screen

--- reactsandbox/src/DatePickerResponsive.js
+++ reactsandbox/src/DatePickerResponsive.js
@@ -1,34 +1,49 @@
 import React, { Fragment, useState } from "react";
-import { BasePicker, MuiPickersUtilsProvider, TimePickerView, Calendar } from "material-ui-pickers";
+import { BasePicker, MuiPickersUtilsProvider, TimePickerView, Calendar, DatePicker } from "material-ui-pickers";
 import moment from "moment";
 import MomentUtils from "@date-io/moment";
 import { Paper } from "@material-ui/core/";
+
+
+function disableWeekends(date) {
+  return 0;
+}
 
 function DatePickerResponsive(props) {
   const [selectedDate, handleDateChange] = useState(new Date());
 
   return (
     <MuiPickersUtilsProvider utils={MomentUtils} moment={moment}>
-        <BasePicker value={selectedDate} onChange={handleDateChange}>
-          {({
-            date,
-            handleAccept,
-            handleChange,
-            handleClear,
-            handleDismiss,
-            handleSetTodayDate,
-            handleTextFieldChange,
-            pick12hOr24hFormat,
-          }) => (
-            <div>
-              <div className="picker">
-                <Paper style={{ overflow: "hidden" }}>
-                  <Calendar date={date} onChange={handleChange} />
-                </Paper>
+      <div>
+          <BasePicker value={selectedDate} onChange={handleDateChange}>
+            {({
+              date,
+              handleAccept,
+              handleChange,
+              handleClear,
+              handleDismiss,
+              handleSetTodayDate,
+              handleTextFieldChange,
+              pick12hOr24hFormat,
+            }) => (
+              <div>
+                <div className="picker">
+                  <Paper style={{ overflow: "hidden" }}>
+                    <Calendar date={date} onChange={handleChange} />
+                  </Paper>
+                </div>
               </div>
-            </div>
-          )}
-        </BasePicker>
+            )}
+          </BasePicker>
+        </div>
+        <div className="picker">
+          <DatePicker
+            label="Basic example"
+            value={selectedDate}
+            onChange={handleDateChange}
+            animateYearScrolling
+          />
+        </div>
     </MuiPickersUtilsProvider>
   );
 }
